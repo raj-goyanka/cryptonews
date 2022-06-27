7a29faddffff5f19733f6afa924361d5b9f02a23
Raj Goyanka <rajgoyanka51@gmail.com>
2022-06-27 21:47:36 +0530

    Final Commit after fixing CSRF Bug

--- cryptonews/settings.py
+++ cryptonews/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT=BASE_DIR / 'static'
